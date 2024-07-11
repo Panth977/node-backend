fadc919dda6e773d648c9eba39b33e82d5a7d115
Panth <panth977@Panths-MacBook-Pro.local>
2024-07-11 15:38:02 +0530

    dispose feature added

--- src/functions/context.ts
+++ src/functions/context.ts
@@ -3,41 +3,58 @@ import { randomUUID } from 'crypto';
 export type Context = {
     id: string;
     log(message: string, meta?: unknown): void;
-    onDispose: (exe: (context: Omit<Context, 'onDispose' | 'dispose'>) => Promise<void>) => void;
+    onDispose: (exe: () => Promise<void>) => void;
     dispose: () => Promise<void>;
     getStack(): string | undefined;
 };
 export type BuildContext<C extends Context> = (context: Context | null) => C;
-let defaultLogger = function (context: Omit<Context, 'log'>, args: unknown[]) {
-    console.log(...args);
-};
-export function setDefaultLogger(logger: typeof defaultLogger) {
-    defaultLogger = logger;
-}
-let defaultOnDisposeExe: Parameters<Context['onDispose']>[0] | null = null;
-export function setDefaultOnDisposeExe(exe: typeof defaultOnDisposeExe) {
-    defaultOnDisposeExe = exe;
-}
-export const DefaultBuildContext: BuildContext<Context> = function (context) {
-    if (context) return Object.assign({}, context, { params: undefined });
-    const dispose: Parameters<Context['onDispose']>[0][] = [];
-    if (defaultOnDisposeExe) dispose.push(defaultOnDisposeExe);
-    return {
-        id: randomUUID(),
-        log(...args) {
-            defaultLogger(this, args);
-        },
-        onDispose(exe) {
-            dispose.push(exe);
-        },
-        async dispose() {
-            await Promise.allSettled([...dispose].reverse().map((exe) => exe(this)));
-        },
-        getStack() {
-            return new Error().stack?.substring(5);
-        },
+export const DefaultBuildContext = (function () {
+    type Logger = (context: Omit<Context, 'log'>, args: unknown[]) => void;
+    type OnDisposeExe = (context: Omit<Context, 'onDispose' | 'dispose'>) => Promise<void>;
+    const loggers = new Set<Logger>();
+    const onDisposeExes = new Set<OnDisposeExe>();
+    function addLogger(logger: Logger) {
+        loggers.add(logger);
+        return function () {
+            loggers.delete(logger);
+        };
+    }
+    function addOnDisposeExe(onDisposeExe: OnDisposeExe) {
+        onDisposeExes.add(onDisposeExe);
+        return function () {
+            onDisposeExes.delete(onDisposeExe);
+        };
+    }
+    const DefaultBuildContext: BuildContext<Context> = function (context) {
+        if (context) return Object.assign({}, context, { params: undefined });
+        const dispose: Parameters<Context['onDispose']>[0][] = [];
+        return {
+            id: randomUUID(),
+            log(...args) {
+                if (loggers.size) {
+                    for (const fn of loggers) {
+                        fn(this, args);
+                    }
+                } else {
+                    console.log(...args);
+                }
+            },
+            onDispose(exe) {
+                dispose.push(exe);
+            },
+            async dispose() {
+                await Promise.allSettled([...dispose].reverse().map((exe) => exe()));
+                for (const fn of onDisposeExes) {
+                    fn(this);
+                }
+            },
+            getStack() {
+                return new Error().stack?.substring(5);
+            },
+        };
     };
-};
+    return Object.assign(DefaultBuildContext, { addLogger, addOnDisposeExe });
+})();
 export function BuildContextWithParamsBuilder<P, C extends Context>(params: P, buildContext: BuildContext<C>): BuildContext<C & { params: P }> {
     return function (context) {
         return Object.assign(buildContext(context), { params: params });
