1a9841749ae3c646903883084826f1d6dbd35bf4
Panth <panth977@Panths-MacBook-Pro.local>
2024-07-13 08:53:48 +0530

    bug fix

--- src/logs/file.ts
+++ src/logs/file.ts
@@ -6,22 +6,26 @@ export function createFileLogger(filepath: string, separator = '\n', options: { 
         logWriteStream.write(log + separator);
     }
     function onInterval() {
-        if (!fs.existsSync(filepath)) return;
-        const currentTime = Date.now();
-        appendLog(` ---------- TIMESTAMP: ${currentTime} ---------- `);
-        const expiryTimestamp = Date.now() - (options.ttl || 0) * 1000;
-        let chunk = fs.readFileSync(filepath).toString();
-        let match;
-        while ((match = /-----------TIMESTAMP: (\d+) ----------/g.exec(chunk)) !== null) {
-            const timestamp = parseInt(match[1], 10);
-            if (timestamp > expiryTimestamp) {
-                chunk = chunk.substring(match.index);
-                break;
-            } else {
-                chunk = chunk.substring(match.index + match[0].length);
+        try {
+            if (!fs.existsSync(filepath)) return;
+            const currentTime = Date.now();
+            appendLog(` ---------- TIMESTAMP: ${currentTime} ---------- `);
+            const expiryTimestamp = Date.now() - (options.ttl || 0) * 1000;
+            let chunk = fs.readFileSync(filepath).toString();
+            let match;
+            while ((match = /---------- TIMESTAMP: (\d+) ----------/g.exec(chunk)) !== null) {
+                const timestamp = parseInt(match[1], 10);
+                if (timestamp > expiryTimestamp) {
+                    chunk = chunk.substring(match.index);
+                    break;
+                } else {
+                    chunk = chunk.substring(match.index + match[0].length);
+                }
             }
+            fs.writeFileSync(filepath, chunk);
+        } catch (err) {
+            console.error(err);
         }
-        fs.writeFileSync(filepath, chunk);
     }
     if (options.expireDuration && fs.existsSync(filepath)) {
         const stats = fs.statSync(filepath);
