74e28a5e8f9258351e60b7012b5f63d6bd44499c
Panth <panth977@Panths-MacBook-Pro.local>
2024-08-28 13:28:23 +0530

    caching updates, nameing changes

--- src/cache/client/redis.ts
+++ src/cache/client/redis.ts
@@ -27,14 +27,15 @@ export class RedisCacheClient<
     async read(context: Context, params: { key: string; fields?: string[] }[]): Promise<unknown[]> {
         const luaScript = `
         local result = {}
+        local null = cjson.decode('null')
         for i, key in ipairs(KEYS) do
             local fields = cjson.decode(ARGV[i])
             local keyType = redis.call('type', key).ok
-            if keyType == 'string' and not fields then
+            if keyType == 'string' and fields == null then
                 table.insert(result, redis.call('get', key))
-            elseif keyType == 'hash' and fields == "*" then
+            elseif keyType == 'hash' and fields == '*' then
                 table.insert(result, redis.call('hgetall', key))
-            elseif keyType == 'hash' and fields and fields ~= "*" then
+            elseif keyType == 'hash' and fields ~= null and fields ~= '*' then
                 local hashResult = {}
                 for _, field in ipairs(fields) do
                     table.insert(hashResult, field)
@@ -42,7 +43,7 @@ export class RedisCacheClient<
                 end
                 table.insert(result, hashResult)
             else
-                table.insert(result, nil)
+                table.insert(result, null)
             end
         end
         return result
@@ -125,19 +126,21 @@ export class RedisCacheClient<
     }
     async remove(context: Context, params: { key: string; fields?: '*' | string[] }[]): Promise<void> {
         const luaScript = `
+        local null = cjson.decode('null')
         for i, key in ipairs(KEYS) do
             local fields = cjson.decode(ARGV[i])
             local keyType = redis.call('type', key).ok
-            if not fields and keyType == 'string' then
+            if keyType == 'string' and fields == null then
                 redis.call('del', key)
-            elseif fields == "*" and keyType == 'hash' then
+            elseif keyType == 'hash' and fields == '*' then
                 redis.call('del', key)
-            elseif fields and keyType == 'hash' and fields ~= "*" then
+            elseif keyType == 'hash' and fields ~= null and fields ~= '*' then
                 for _, field in ipairs(fields) do
                     redis.call('hdel', key, field)
                 end
             end
         end
+        return 1
         `;
         await this.client.eval(luaScript, {
             keys: params.map((p) => p.key),
