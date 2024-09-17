c4ecbe5ced61c9501cc0ae394b53c162f30a6208
Panth <panth977@Panths-MacBook-Pro.local>
2024-09-17 08:47:47 +0530

    safeparser warraper with debug

--- src/functions/wrapper/parse.ts
+++ src/functions/wrapper/parse.ts
@@ -12,14 +12,17 @@ export function SafeParse<
     O extends z.ZodType,
     L,
     C extends Context,
->(params: AsyncFunction.Params<I, O, L, C>, behavior?: { input?: boolean; output?: boolean }): AsyncFunction.WrapperBuild<I, O, L, C>;
+>(
+    params: AsyncFunction.Params<I, O, L, C>,
+    behavior?: { debug?: boolean; input?: boolean; output?: boolean }
+): AsyncFunction.WrapperBuild<I, O, L, C>;
 export function SafeParse<
     //
     I extends z.ZodType,
     O extends z.ZodType,
     L,
     C extends Context,
->(params: SyncFunction.Params<I, O, L, C>, behavior?: { input?: boolean; output?: boolean }): SyncFunction.WrapperBuild<I, O, L, C>;
+>(params: SyncFunction.Params<I, O, L, C>, behavior?: { debug?: boolean; input?: boolean; output?: boolean }): SyncFunction.WrapperBuild<I, O, L, C>;
 export function SafeParse<
     //
     I extends z.ZodType,
@@ -30,7 +33,7 @@ export function SafeParse<
     C extends Context,
 >(
     params: SyncGenerator.Params<I, Y, N, O, L, C>,
-    behavior?: { input?: boolean; output?: boolean; yield?: boolean; next?: boolean }
+    behavior?: { debug?: boolean; input?: boolean; output?: boolean; yield?: boolean; next?: boolean }
 ): SyncGenerator.WrapperBuild<I, Y, N, O, L, C>;
 export function SafeParse<
     //
@@ -42,87 +45,112 @@ export function SafeParse<
     C extends Context,
 >(
     params: AsyncGenerator.Params<I, Y, N, O, L, C>,
-    behavior?: { input?: boolean; output?: boolean; yield?: boolean; next?: boolean }
+    behavior?: { debug?: boolean; input?: boolean; output?: boolean; yield?: boolean; next?: boolean }
 ): AsyncGenerator.WrapperBuild<I, Y, N, O, L, C>;
-export function SafeParse(params_: unknown, behavior: { input?: boolean; output?: boolean; yield?: boolean; next?: boolean } = {}): WrapperBuild {
+export function SafeParse(
+    params_: unknown,
+    behavior: { debug?: boolean; input?: boolean; output?: boolean; yield?: boolean; next?: boolean } = {}
+): WrapperBuild {
     const params = getParams(params_);
     let Wrapper: undefined | WrapperBuild;
     if (params.type === 'function') {
         Wrapper = function (context, input, func) {
             if (behavior.input ?? true) {
+                const start = Date.now();
                 input = params._input.parse(input, { path: [context.params.getRef() + ':input'] });
+                if (behavior.debug) context.log(`${context.params.getRef()}:input parsed in ${Date.now() - start}`);
             }
             let output = func(context, input);
             if (behavior.output ?? true) {
+                const start = Date.now();
                 output = params._output.parse(output, { path: [context.params.getRef() + ':output'] });
+                if (behavior.debug) context.log(`${context.params.getRef()}:output parsed in ${Date.now() - start}`);
             }
             return output;
         } satisfies SyncFunction.WrapperBuild;
-    }
-    if (params.type === 'async function') {
+    } else if (params.type === 'async function') {
         Wrapper = async function (context, input, func) {
             if (behavior.input ?? true) {
+                const start = Date.now();
                 input = params._input.parse(input, { path: [context.params.getRef() + ':input'] });
+                if (behavior.debug) context.log(`${context.params.getRef()}:input parsed in ${Date.now() - start}`);
             }
             let output = await func(context, input);
             if (behavior.output ?? true) {
+                const start = Date.now();
                 output = params._output.parse(output, { path: [context.params.getRef() + ':output'] });
+                if (behavior.debug) context.log(`${context.params.getRef()}:output parsed in ${Date.now() - start}`);
             }
             return output;
         } satisfies AsyncFunction.WrapperBuild;
-    }
-    if (params.type === 'async function*') {
+    } else if (params.type === 'async function*') {
         Wrapper = async function* (context, input, func) {
             if (behavior.input ?? true) {
+                const start = Date.now();
                 input = params._input.parse(input, { path: [context.params.getRef() + ':input'] });
+                if (behavior.debug) context.log(`${context.params.getRef()}:input parsed in ${Date.now() - start}`);
             }
             const g = func(context, input);
             let val = await g.next();
             while (!val.done) {
                 let y = val.value;
                 if (behavior.yield ?? true) {
+                    const start = Date.now();
                     y = params._yield.parse(y, { path: [context.params.getRef() + ':yield'] });
+                    if (behavior.debug) context.log(`${context.params.getRef()}:yield parsed in ${Date.now() - start}`);
                 }
                 let next = yield y;
                 if (behavior.next ?? true) {
+                    const start = Date.now();
                     next = params._next.parse(next, { path: [context.params.getRef() + ':next'] });
+                    if (behavior.debug) context.log(`${context.params.getRef()}:next parsed in ${Date.now() - start}`);
                 }
                 val = await g.next(next);
             }
             let output = val.value;
             if (behavior.output ?? true) {
+                const start = Date.now();
                 output = params._output.parse(output, { path: [context.params.getRef() + ':output'] });
+                if (behavior.debug) context.log(`${context.params.getRef()}:output parsed in ${Date.now() - start}`);
             }
             return output;
         } satisfies AsyncGenerator.WrapperBuild;
-    }
-    if (params.type === 'function*') {
+    } else if (params.type === 'function*') {
         Wrapper = function* (context, input, func) {
             if (behavior.input ?? true) {
+                const start = Date.now();
                 input = params._input.parse(input, { path: [context.params.getRef() + ':input'] });
+                if (behavior.debug) context.log(`${context.params.getRef()}:input parsed in ${Date.now() - start}`);
             }
             const g = func(context, input);
             let val = g.next();
             while (!val.done) {
                 let y = val.value;
                 if (behavior.yield ?? true) {
+                    const start = Date.now();
                     y = params._yield.parse(y, { path: [context.params.getRef() + ':yield'] });
+                    if (behavior.debug) context.log(`${context.params.getRef()}:yield parsed in ${Date.now() - start}`);
                 }
                 let next = yield y;
                 if (behavior.next ?? true) {
+                    const start = Date.now();
                     next = params._next.parse(next, { path: [context.params.getRef() + ':next'] });
+                    if (behavior.debug) context.log(`${context.params.getRef()}:next parsed in ${Date.now() - start}`);
                 }
                 val = g.next(next);
             }
             let output = val.value;
             if (behavior.output ?? true) {
+                const start = Date.now();
                 output = params._output.parse(output, { path: [context.params.getRef() + ':output'] });
+                if (behavior.debug) context.log(`${context.params.getRef()}:output parsed in ${Date.now() - start}`);
             }
             return output;
         } satisfies SyncGenerator.WrapperBuild;
+    } else {
+        throw new Error('Unimplemented!');
     }
-    if (Wrapper) return Object.assign(Wrapper, { [instance]: SafeParse });
-    throw new Error('Unimplemented!');
+    return Object.assign(Wrapper, { [instance]: SafeParse });
 }
 const instance = Symbol();
 export function isSafeParse<W>(w: W): boolean {
